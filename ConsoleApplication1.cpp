49bb26bbc90bfa39c283d8871b8251a02f63e01a
VladimirMironov <44780782+vladimirVFX@users.noreply.github.com>
2020-05-12 11:31:52 +0300

    Add files via upload

---
+++ ConsoleApplication1.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+#include "vulkan\vulkan.h"
+
+using namespace std;
+
+#define ASSERT_VULKAN(val)\
+    if(val != VK_SUCCESS){\
+        __debugbreak();\
+    }
+
+VkInstance instance;
+
+void printStats(VkPhysicalDevice & device) {
+    VkPhysicalDeviceProperties properties;
+    vkGetPhysicalDeviceProperties(device, &properties);
+    cout << "DeviceName: " << properties.deviceName << endl;
+    cout << "API Version: " << properties.apiVersion << endl;
+    cout << "DeviceID: " << properties.deviceID << endl;
+    cout << "DeviceType: " << properties.deviceType << endl;
+    cout << "DriverVersion: " << properties.driverVersion << endl;
+    cout << "VendorID: " << properties.vendorID << endl;
+
+    cout << endl;
+}
+
+int main()
+{   
+    VkApplicationInfo appInfo;
+    appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+    appInfo.pNext = NULL;
+    appInfo.pApplicationName = "Vulkan Tutorial";
+    appInfo.applicationVersion = VK_MAKE_VERSION(0,0,0);
+    appInfo.pEngineName = "Vulkan Version Engine";
+    appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
+    appInfo.apiVersion = VK_API_VERSION_1_0;
+
+    VkInstanceCreateInfo instanceInfo = {};
+    
+    instanceInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+    instanceInfo.pNext = NULL;
+    instanceInfo.flags = 0;
+    instanceInfo.pApplicationInfo = &appInfo;
+    instanceInfo.enabledLayerCount = 0;
+    instanceInfo.ppEnabledLayerNames = NULL;
+    instanceInfo.enabledExtensionCount = 0;
+
+    VkResult result = vkCreateInstance(&instanceInfo, NULL, &instance);
+
+    ASSERT_VULKAN(result);
+
+    uint32_t amountOfPhysicalDevices = 0;
+    result = vkEnumeratePhysicalDevices(instance, &amountOfPhysicalDevices, NULL);
+    ASSERT_VULKAN(result);
+
+    VkPhysicalDevice *physicalDevices = new VkPhysicalDevice[amountOfPhysicalDevices];
+    result = vkEnumeratePhysicalDevices(instance, &amountOfPhysicalDevices, physicalDevices);
+    ASSERT_VULKAN(result);
+     
+    for (int i = 0; i < amountOfPhysicalDevices; i++) {
+        printStats(physicalDevices[i]);
+    }
+
+    return 0; 
+}
+
+
